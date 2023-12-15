87d473f6e9bc777eecf9deb0066aa940afac8be0
Jon Dawson <chips@jondawson.org.uk>
2023-12-15 21:05:52 +0000

    Attempt at DMA based phase correction

--- 17_frequency_generator/frequency_generator.cpp
+++ 17_frequency_generator/frequency_generator.cpp
@@ -1,71 +1,72 @@
 #include <stdio.h>
+#include <cmath>
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "hardware/dma.h"
 // Our assembled program:
 #include "stream_bits.pio.h"
 
-uint32_t bit_buffer[256] __attribute__((aligned(1024))) ={
-0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,
-0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,
-0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,
-0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,
+void nco_start(double frequency_Hz)
+{
+    //calculate some constants
+    const double normalised_frequency = frequency_Hz/125e6;
+    const double period_clocks = 125e6/frequency_Hz;
 
-0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,
-0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,
-0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,
-0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,
+    //this gives a maximim period of 256 clocks and minimum of 2, that 488.28125kHz to 62.5MHz
+    static const uint16_t waveform_length_bits = 256u; 
+    static const uint16_t waveform_length_words = waveform_length_bits/32;
+    const uint32_t fraction_bits = 24u;
 
-0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,
-0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,
-0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,
-0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,
+    //Each 256 bit waveform will not necassarily be a whole number of cycles, we will probably be part way through a cycle at the end.
+    //This constant tells us how many clocks (and part clocks) we were through the last cycle at the end of a 256 bit waveform.
+    //Next time, we chose a waveform that has been advanced by this many clocks so that we pick up where we left off (to the nearest clock).
+    //The the fraction part is accumulated so that the any rounding error weintroduce can be corrected for later.
+    //8 integer bits allow up to 256 whole clocks of advancement, the 24 remaining bit are fraction bits.
+    const int32_t index_increment_f24 = round(fmod(waveform_length_bits, period_clocks) * pow(2.0, fraction_bits));
 
-0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,
-0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,
-0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,
-0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,   0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,
-};
+    //store 32 waveforms 
+    //32 copies of the waveform are stored, each advanced by 1 clock more than the last
+    //advancements of up to 31 bits are achieved by selecting the appriopriatly advanced waveform.
+    //advancements of 32 bits or more can be achieved by advancing whole words through each waveform.
+    static uint32_t buffer[32*waveform_length_words*2] __attribute__((aligned(4))); 
 
-PIO pio = pio0;
-uint sm = pio_claim_unused_sm(pio, true);
-int nco_dma;
-dma_channel_config nco_dma_cfg;
-int chain_dma;
-dma_channel_config chain_dma_cfg;
-
-void dma_handler() {
-
-
-    //When a DMA completes, reconfigure it but don't start it yet, wait for DMA chaining to trigger
-    if(dma_hw->ints0 & (1u << nco_dma))
+    //Uses floating point arithmetic and trig functions.
+    //Not very fast but doesn't matter because it only runs once.
+    for(uint8_t advance = 0u; advance < 32; ++advance)
     {
-      printf("a\n");
-      dma_channel_configure(nco_dma, &nco_dma_cfg, &pio->txf[sm], bit_buffer, 256, false);
-      dma_hw->ints0 = 1u << nco_dma; //acknowledge interrupt
+      uint32_t sample_number = advance;
+      for(uint16_t word=0; word<waveform_length_words*2; ++word)
+      {
+        uint32_t bit_samples = 0;
+        for(uint8_t bit=0; bit<32; ++bit)
+        {
+          double sample = sin(2 * M_PI * normalised_frequency * sample_number++);
+          //could apply dithering here to remove harmonics
+          //i.e. sample += (((double)rand()/(double)RANDMAX) - 0.5) * 2.0 //random number between -1 and +1
+          if(sample > 0)
+          {
+            bit_samples |= (1 << bit);
+          }  
+        }
+        buffer[(advance*waveform_length_words*2) + word] = bit_samples;
+      }
     }
 
-}
-
-int main() {
-  stdio_init_all();
-  stdio_set_translate_crlf(&stdio_usb, false);
-
-    // Choose which PIO instance to use (there are two instances)
+    //The PIO contains a very simple program that reads a 32 bit word
+    //from the FIFO and sends 1 bit per clock to an IO pin
     PIO pio = pio0;
-
-    // Our assembled program needs to be loaded into this PIO's instruction
-    // memory. This SDK function will find a location (offset) in the
-    // instruction memory where there is enough space for our program. We need
-    // to remember this location!
-    uint offset = pio_add_program(pio, &stream_bits_program);
-
-    // Find a free state machine on our chosen PIO (erroring if there are
-    // none). Configure it to run our program, and start it, using the
-    // helper function we included in our .pio file.
     uint sm = pio_claim_unused_sm(pio, true);
-    stream_bits_program_init(pio, sm, offset, 0);
+    uint offset = pio_add_program(pio, &stream_bits_program);
+    sm = pio_claim_unused_sm(pio, true);
+    stream_bits_program_init(pio, sm, offset, 0); //GPIO0
 
+    //2 DMA channels are used.
+    //1 transfers the pre-generated blocks of 256-bits to PIO
+    //The second DMA configures the first from a table of start addresses
+    int nco_dma;
+    dma_channel_config nco_dma_cfg;
+    int chain_dma;
+    dma_channel_config chain_dma_cfg;
     nco_dma = dma_claim_unused_channel(true);
     chain_dma = dma_claim_unused_channel(true);
 
@@ -76,74 +77,81 @@ int main() {
     channel_config_set_write_increment(&nco_dma_cfg, false);
     channel_config_set_dreq(&nco_dma_cfg, pio_get_dreq(pio, sm, true));
     channel_config_set_chain_to(&nco_dma_cfg, chain_dma);
-    channel_config_set_ring(&nco_dma_cfg, false, 10); // 1 << 10 = 1024 bytes = 256 ints read addresses wrap
-    dma_channel_configure(nco_dma, &nco_dma_cfg, &pio->txf[sm], bit_buffer, 256, false);
+    channel_config_set_irq_quiet(&nco_dma_cfg, true); //don't generate interrupt for each transfer, only on NULL transfer
+    dma_channel_configure(nco_dma, &nco_dma_cfg, &pio->txf[sm], NULL, 8, false); //8x32bit transfers
     
     //use a second DMA to reconfigure the first
     chain_dma_cfg = dma_channel_get_default_config(chain_dma);
     channel_config_set_transfer_data_size(&chain_dma_cfg, DMA_SIZE_32);
-    channel_config_set_read_increment(&chain_dma_cfg, false); //no increment, avoid the need to reprogram
-    channel_config_set_write_increment(&chain_dma_cfg, false);
-    channel_config_set_chain_to(&chain_dma_cfg, nco_dma);
+    channel_config_set_read_increment(&chain_dma_cfg, true); //works through a list of transfers
+    channel_config_set_write_increment(&chain_dma_cfg, false); //always writes to data DMA read address
+    dma_channel_start(nco_dma);
 
-    //second DMA doesn't transfer any data, it just restarts the first
-    uint32_t null_transfer_data;
-    dma_channel_configure(
-      chain_dma,
-      &chain_dma_cfg,
-      &null_transfer_data, // Initial write address
-      &null_transfer_data, // Initial read address
-      1, // Halt after each control block
-      false // Don't start yet
-    );
+    //Create a table of addresses to send
+    const uint32_t* buffer_addresses[2][33];
+    uint32_t index_f24 = 0u;
+    buffer_addresses[0][33] = NULL; //null transfer at end of each 32 address block
+    buffer_addresses[1][33] = NULL;
+    uint8_t ping_pong = 0u;
 
-    //DMA triggers an interrupt upon completion
-    //dma_set_irq0_channel_mask_enabled((1u<<nco_dma) | (1u<<chain_dma), true);
-    ////irq_set_exclusive_handler(DMA_IRQ_0, dma_handler);
-    ////irq_set_enabled(DMA_IRQ_0, true);
+    //plan first 32 DMA transfers
+    for(uint8_t transfer = 0u; transfer < 32u; ++transfer)
+    {
+      index_f24 += index_increment_f24;
+      uint8_t index = index_f24 >> fraction_bits;
+      //The lower 5 bits represent up to 31 bits of advance
+      //The upper 3 bits represent the number of whole words of advance
+      //we need to swap these because the words of each waveform are contiguous
+      index = (index << 5) | (index >> 3);
+      buffer_addresses[ping_pong][transfer] = &buffer[index];
+    }
+    
+    //Play back 32, 256-bit chunks of the pre-generated waveform at a time.
+    //The CPU calculates the correct delay for each chunk.
+    while(1) //Loop iterates at 125e6/(256*32) = 15259 Hz
+    {
 
-    sleep_us(10000000);
-    printf("starting\n");
-    //dma_channel_start(nco_dma);
-    dma_start_channel_mask(1u << nco_dma);
+      //don't put new code here!!
+      //second DMA overwrite read address and triggers nco DMA
+      dma_channel_configure(
+        chain_dma,
+        &chain_dma_cfg,
+        &dma_hw->ch[nco_dma].al3_read_addr_trig, //overwrite nco_dma read address and trigger new transfer
+        &buffer_addresses[ping_pong][0], //increments each time
+        1,   // Halt after 1 transfer
+        true // start now
+      );
+      ping_pong ^= 1;
 
-    // The state machine is now running. Any value we push to its TX FIFO will
-    // appear on the LED pin.
-    while (true) {}
+      //plan next 32 tranfers while last 32 are sending
+      for(uint8_t transfer = 0u; transfer < 32u; ++transfer)
+      {
+        index_f24 += index_increment_f24;
+        uint8_t index = index_f24 >> fraction_bits;
+        //The lower 5 bits represent up to 31 bits of advance
+        //The upper 3 bits represent the number of whole words of advance
+        //we need to swap these because the words of each waveform are contiguous
+        index = (index << 3) | (index >> 5);
+        buffer_addresses[ping_pong][transfer] = &buffer[index];
+      }
 
-    //configure PIO
-    //uint offset = pio_add_program(pio, &stream_bits_program);
-    //stream_bits_program_init(pio, sm, offset, 3);
+      //don't plan any more until a block of 32 completes
+      dma_hw->ints0 = 1u << nco_dma;
+      while (!(dma_hw->intr & 1u << nco_dma)) 
+      tight_loop_contents();
+      //PIO FIFO has 8 entries, so need to kick off next DMA within 32 x 8 = 256 
+      //clocks of last one finishing don't put anything between here and next DMA
 
-    //confgure DMA from memory to PIO TX FIFO
-    //nco_dma = dma_claim_unused_channel(true);
-    //nco_dma_cfg = dma_channel_get_default_config(nco_dma);
-    //channel_config_set_transfer_data_size(&nco_dma_cfg, DMA_SIZE_32);
-    //channel_config_set_read_increment(&nco_dma_cfg, true);
-    //channel_config_set_write_increment(&nco_dma_cfg, false);
-    //channel_config_set_dreq(&nco_dma_cfg, pio_get_dreq(pio, sm, true));
-    //channel_config_set_chain_to(&nco_dma_cfg, chain_dma);
-    //dma_channel_configure(nco_dma, &nco_dma_cfg, bit_buffer, &pio->txf[sm], 256, false);
+    }
+}
 
-    //chain_dma = dma_claim_unused_channel(true);
-    //chain_dma_cfg = dma_channel_get_default_config(chain_dma);
-    //channel_config_set_transfer_data_size(&chain_dma_cfg, DMA_SIZE_32);
-    //channel_config_set_read_increment(&chain_dma_cfg, true);
-    //channel_config_set_write_increment(&chain_dma_cfg, false);
-    //channel_config_set_dreq(&chain_dma_cfg, pio_get_dreq(pio, sm, true));
-    //channel_config_set_chain_to(&chain_dma_cfg, nco_dma);
-    //dma_channel_configure(chain_dma, &chain_dma_cfg, bit_buffer, &pio->txf[sm], 256, false);
 
-    //DMA triggers an interrupt upon completion
-    //dma_set_irq0_channel_mask_enabled((1u<<nco_dma) | (1u<<chain_dma), true);
-    //irq_set_exclusive_handler(DMA_IRQ_0, dma_handler);
-    //irq_set_enabled(DMA_IRQ_0, true);
+int main() {
+  stdio_init_all();
+  stdio_set_translate_crlf(&stdio_usb, false);
 
-    //sleep_us(10000000);
-    //while(1){
-    //  printf("start\n");
-    //  dma_start_channel_mask(1u << nco_dma);
-    //  printf("stop\n");
-   // }
+  sleep_us(10000000);
+  printf("starting\n");
+  nco_start(1e6);
 
 }

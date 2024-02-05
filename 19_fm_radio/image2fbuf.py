9ba613f3c2f92caa837d8d8f2a0b1a6809811b42
Jon Dawson <chips@jondawson.org.uk>
2024-02-05 20:29:02 +0000

    Add fm radio example

---
+++ 19_fm_radio/image2fbuf.py
@@ -0,0 +1,35 @@
+#  _  ___  _   _____ _     _                 
+# / |/ _ \/ | |_   _| |__ (_)_ __   __ _ ___ 
+# | | | | | |   | | | '_ \| | '_ \ / _` / __|
+# | | |_| | |   | | | | | | | | | | (_| \__ \
+# |_|\___/|_|   |_| |_| |_|_|_| |_|\__, |___/
+#                                  |___/    
+#
+# Copyright (c) Jonathan P Dawson 2024
+# filename: image2fbuf.py
+# description:
+# License: MIT
+#
+
+import imageio
+import sys
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+im = imageio.imread(input_file)
+h, w, c = im.shape
+      
+bytevals = []
+for y in range(h):
+  for i in range(w//8):
+    byteval = 0
+    for j in range(8):
+      x = (i*8)+j   
+      byteval <<= 1
+      if im[y][x][0] > 0:
+        byteval |= 1
+    bytevals.append(byteval) 
+
+with open(output_file, "wb") as outf:
+  outf.write(bytes(bytevals))
